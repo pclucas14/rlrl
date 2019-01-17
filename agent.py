8041db4656faf7ea13c61de209774d158053743a
Lucas <pc.lucas14@gmail.com>
2019-01-17 15:58:55 +0200

    bug fix + aliasing

--- agent.py
+++ agent.py
@@ -1,55 +1,83 @@
 import numpy as np
 from utils import pprint
 
+
 '''
 our agent
 '''
 
-class V_net(object):
+class Aliaser(object):
     def __init__(self, args, env):
+        
+        # aliasing setup for POMDP
+        assert args.alias_percentage == 0 or 'chain' in args.env.lower()
+        
+        self.state_mapping = {i:i for i in range(env.observation_space.n)}
+        chain_length = (env.observation_space.n - 1) // 3
+        no_aliased_states = int(args.alias_percentage / 100. * chain_length)
+        self.aliased_indices = np.random.choice(chain_length, no_aliased_states)
+        self.aliased_indices += chain_length + 1
+        
+        for aliased_index in self.aliased_indices:
+            self.state_mapping[aliased_index] = \
+                    aliased_index + chain_length
+
+    def __call__(self, index):
+        return self.state_mapping[index]
+
+
+class V_net(object):
+    def __init__(self, args, env, aliaser):
         self.trace  = np.zeros((env.observation_space.n, ))    
-        self.values = np.zeros((env.observation_space.n, ))   
+        self.values = np.zeros((env.observation_space.n, )) + args.init_value 
         self.args   = args
         self.env    = env
- 
+        self.ali    = aliaser
+
+        # mask out aliased indices for clarity when printing
+        self.trace[self.ali.aliased_indices] = -1
+    
     def update_trace(self, state, beta):
-        self.trace = (1 - beta) * self.trace
-        self.trace[state] += beta 
+        self.trace = (1. - beta) * self.trace
+        self.trace[self.ali(state)] += beta
+
+    def reset_trace(self):
+        self.trace *= 0. 
 
     def update_values(self, td_error):
         self.values += self.args.lr * td_error * self.trace
 
     # fetch state value
     def __call__(self, x):
-        return self.values[x]
+        return self.values[self.ali(x)]
 
     def __str__(self):
-       return pprint(self.values.reshape(self.env.shape))
+       values = self.values
+       values[self.ali.aliased_indices] = -9.99
+       return pprint(values.reshape(self.env.shape))
 
      
 class B_net(object):
-    def __init__(self, args, env):
+    def __init__(self, args, env, aliaser):
         self.b_logits = np.zeros((env.observation_space.n, ))
         self.args     = args
         self.env      = env
+        self.ali      = aliaser
 
     def stable_sigmoid(self, x):
         return np.exp(-np.logaddexp(0, -x)) 
 
-    def clip(self, x):
-        return max(-self.args.clip, min(self.args.clip, x))
-    
     def update_logits(self, memory):
         for (state, sig, target, v_tilde, v_tilde_prev, v) in memory:
             grad = sig * (1 - sig) * ((v_tilde - target) * (v - v_tilde_prev) + self.args.vc)
-            self.b_logits[state] -= self.args.beta_lr * self.clip(grad)
+            self.b_logits[self.ali(state)] -= self.args.beta_lr * grad 
 
     # fetch beta value
     def __call__(self, x):
-        return self.stable_sigmoid(self.b_logits)[x]
+        return self.stable_sigmoid(self.b_logits)[self.ali(x)]
 
     def __str__(self):
-        return pprint(self.stable_sigmoid(self.b_logits).reshape(self.env.shape))
+        values = self.stable_sigmoid(self.b_logits)
+        values[self.ali.aliased_indices] = -9.99
+        return pprint(values.reshape(self.env.shape))
         
-        
-
--- main.py
+++ main.py
@@ -9,15 +9,17 @@ from agent import *
 
 # arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--env',        type=str,   default='cliff')
-parser.add_argument('--lr',         type=float, default=0.001)
-parser.add_argument('--beta_lr',    type=float, default=0.001)
+parser.add_argument('--env',        type=str,   default='chain')
+parser.add_argument('--lr',         type=float, default=0.1)
+parser.add_argument('--beta_lr',    type=float, default=0.05)
+parser.add_argument('--init_value', type=float, default=0)
 parser.add_argument('--vc',         type=float, default=0.)
-parser.add_argument('--gamma',      type=float, default=0.9)
+parser.add_argument('--gamma',      type=float, default=1.)
 parser.add_argument('--n_episodes', type=int,   default=100000)
 parser.add_argument('--clip',       type=float, default=999)
 parser.add_argument('--update_beta_every', type=int, default=1)
 parser.add_argument('--print_every',       type=int, default=1000)
+parser.add_argument('--alias_percentage',  type=float, default=0)
 args = parser.parse_args()
 
 # environment creation 
@@ -26,17 +28,21 @@ if 'cliff' in args.env.lower():
     env = CliffWalkingEnv()
     env.shape = (4, 12)
 elif 'chain' in args.env.lower():
-    raise NotImplementedError
+    from lib.envs.y_chain import YChain
+    env = YChain()
+    env.shape = (1, -1)
 else: 
     raise ValueError
 
 # create estimators
-v_net = V_net(args, env)
-b_net = B_net(args, env)
+aliaser = Aliaser(args, env) 
+v_net  = V_net(args, env, aliaser)
+b_net  = B_net(args, env, aliaser)
 
 for episode in range(args.n_episodes):
     done = False
     state = env.reset()
+    v_net.reset_trace()
     v_tilde_prev = 0.
     t = 0    
 
@@ -49,7 +55,7 @@ for episode in range(args.n_episodes):
         v    = v_net(state)      
 
         # build on smoothed estimate
-        v_tilde = beta * v + (1 - beta) * v_tilde_prev
+        v_tilde = beta * v + (1. - beta) * v_tilde_prev
         
         # update the trace
         v_net.update_trace(state, beta)
@@ -80,8 +86,7 @@ for episode in range(args.n_episodes):
         v_tilde_prev = v_tilde - reward
         t += 1
 
-
-    if (episode + 1) % args.print_every == 0: 
-        print('values\n', v_net)
-        print('betas \n', b_net)
+    if (episode + 1) % args.print_every == 0:
+        print('values\n'); print(v_net)
+        print('betas \n'); print(b_net)
        
